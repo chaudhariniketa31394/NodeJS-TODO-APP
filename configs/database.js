257d097a826789e5ed297a6569de293115171f65
Niketa Chaudhari <niketac@excellonsoft.com>
2023-02-27 11:17:59 +0530

    first commit

---
+++ configs/database.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+    uri: 'mongodb+srv://todo-api:hZkweFmNN2Lq1cJD@cluster0.lwtdotm.mongodb.net/todo-app?retryWrites=true&w=majority',
+    options: {
+        dbName: 'todo-app',
+        useNewUrlParser: true
+    },
+}
+
+// DB_USER=todo-api
+// DB_PWD=hZkweFmNN2Lq1cJD
+// DB_HOST=cluster0.lwtdotm.mongodb.net
+// DB_NAME=todo
+
+//mongodb+srv://todo-api:hZkweFmNN2Lq1cJD@$cluster0.lwtdotm.mongodb.net/todo?retryWrites=true&w=majority
